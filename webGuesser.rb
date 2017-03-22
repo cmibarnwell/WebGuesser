589371ce9bc9f79a9daee926c2ba183a574259c9
cmibarnwell <calebbarnwell@gmail.com>
2017-03-22 15:08:27 -0500

    Completed Guessing Game

--- webGuesser.rb
+++ webGuesser.rb
@@ -1,8 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+#Create our secret number
 num = rand(100)
 
 get '/' do
-  erb :index, :locals => {:num => num}
+  #Gather our guess value and convert it to an integer
+  guess = params["guess"].to_i
+  #Check secret number against guess value and generate response
+  msg = checkGuess(guess, num)
+  #Call our index with parameters
+  erb :index, :locals => {:num => num, :msg => msg}
+end
+
+
+def checkGuess(guess, num)
+  if (num+5) >= guess && guess > num
+    msg = "Too high!"
+  elsif (num-5) <= guess && guess < num
+    msg = "Too low!"
+  elsif guess > (num + 5)
+    msg = "Way too high!"
+  elsif guess < (num - 5)
+    msg = "Way too low!"
+  else
+    msg = "You got it right!"
+  end
+  return msg
 end
