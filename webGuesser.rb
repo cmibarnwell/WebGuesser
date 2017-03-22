cd6d982845fede541b4b584ff36a02435981cf33
cmibarnwell <calebbarnwell@gmail.com>
2017-03-22 15:40:44 -0500

    Added heading and fixed homepage issue

--- webGuesser.rb
+++ webGuesser.rb
@@ -2,20 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 #Create our secret number
-num = rand(100)
+num = rand(1..100)
 
 get '/' do
+
   #Gather our guess value and convert it to an integer
   guess = params["guess"].to_i
+
   #Check secret number against guess value and generate response
   msg = checkGuess(guess, num)
+
   #Call our index with parameters
   erb :index, :locals => {:num => num, :msg => msg}
 end
 
 
 def checkGuess(guess, num)
-  if (num+5) >= guess && guess > num
+  if guess == 0
+    msg = ""
+  elsif (num+5) >= guess && guess > num
     msg = "Too high!"
   elsif (num-5) <= guess && guess < num
     msg = "Too low!"
