e162d50c768c9be0916be71f0c2f91bdb4b43510
cmibarnwell <calebbarnwell@gmail.com>
2017-03-22 19:39:18 -0500

    Added guess limits and home button

--- webGuesser.rb
+++ webGuesser.rb
@@ -1,8 +1,26 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-#Create our secret number
-num = rand(1..100)
+class Guess
+  @@count = 5
+  #Create our secret number
+  @@num = rand(1..100)
+  def self.get_count
+    return @@count
+  end
+  def self.get_num
+    return @@num
+  end
+  def self.countSubtract
+    @@count -= 1
+  end
+  def self.countReset
+    @@count = 5
+  end
+  def self.newNum
+    @@num = rand(1..100)
+  end
+end
 
 get '/' do
 
@@ -10,26 +28,41 @@ get '/' do
   guess = params["guess"].to_i
 
   #Check secret number against guess value and generate response
-  msg = checkGuess(guess, num)
+  msg = checkGuess(guess)
 
   #Call our index with parameters
-  erb :index, :locals => {:num => num, :msg => msg}
+  num = Guess.get_num
+  count = Guess.get_count
+  erb :index, :locals => {:num => num, :msg => msg, :count  => count}
 end
 
 
-def checkGuess(guess, num)
+def checkGuess(guess)
   if guess == 0
     msg = ""
-  elsif (num+5) >= guess && guess > num
+    return msg
+  elsif (Guess.get_num+5) >= guess && guess > Guess.get_num
     msg = "Too high!"
-  elsif (num-5) <= guess && guess < num
+  elsif (Guess.get_num-5) <= guess && guess < Guess.get_num
     msg = "Too low!"
-  elsif guess > (num + 5)
-    msg = "Way too high!"
-  elsif guess < (num - 5)
+  elsif guess > (Guess.get_num + 5)
+    msg = "Way too high! #{Guess.get_num}"
+  elsif guess < (Guess.get_num - 5)
     msg = "Way too low!"
   else
-    msg = "You got it right!"
+    msg = "You got it right! A new number has been generated if you'd like to play again."
+    Guess.newNum
+    Guess.countReset
   end
+
+  Guess.countSubtract
+
+  if Guess.get_count == 0
+    temp = Guess.get_num
+    Guess.newNum
+    Guess.countReset
+    return "No more guesses! Try again. A new number has been generated. Number was #{temp}"
+  end
+
   return msg
 end
